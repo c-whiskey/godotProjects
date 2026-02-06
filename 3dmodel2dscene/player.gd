b1a5afe289c93ba8730984acdc16db3c5b8905fc
chris <chriswwhite9@hotmail.com>
2026-02-07 08:09:50 +1100

    Add 3d to 2d model project

---
+++ 3dmodel2dscene/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@onready var playerAnim := $SubViewportContainer/SubViewport/Running/AnimationPlayer2
+@onready var player := $SubViewportContainer/SubViewport/Running
+
+func _ready() -> void:
+	playerAnim.play("runningAnimation/mixamo_com")
+	pass
+
+var speed = 3
+
+func _process(delta: float) -> void:
+	player.rotate_y(PI * delta * delta)
+	
+	
+	if Input.is_key_pressed(KEY_A):
+		#player.rotate_y(PI * delta * delta)
+		player.look_at(Vector3.RIGHT)
+		position.x -= speed
+		pass
+	if Input.is_key_pressed(KEY_D):
+		player.look_at(Vector3.LEFT)
+		position.x += speed
+		pass
+	if Input.is_key_pressed(KEY_W):
+		position.y -= speed
+		pass
+	if Input.is_key_pressed(KEY_S):
+		position.y += speed
+		pass
